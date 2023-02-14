5bd76efc57a85317116d26b3953cbc96f5b134bd
pavel <9655744573x@gmail.com>
2023-02-14 09:16:30 +0500

    Step2.2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,11 +4,7 @@
 package hexlet.code;
 
 public class App {
-    public String getGreeting() {
-        return "Welcome to the Brain Games!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println("Welcome to the Brain Games!");
     }
 }
